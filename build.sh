9d14e37004951003c7c47c2a154ad7e7e9bc53a5
MISHA <185427799+sprytex@users.noreply.github.com>
2025-01-10 00:49:00 +0400

    to build it

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+chmod 701 remap.sh
+chmod 701 decompile.sh
+chmod 701 init.sh
+chmod 701 newApplyPatches.sh
+
 git submodule update --init && ./remap.sh && ./decompile.sh && ./init.sh && ./newApplyPatches.sh && mvn clean install
